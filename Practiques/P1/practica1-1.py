49e002ed85f807d84b6eaa3b99345d3f49df73e9
lorenaobiol <lorenacurto7@gmail.com>
2026-03-05 17:01:05 +0100

    5326

--- Practiques/P1/practica1-1.py
+++ Practiques/P1/practica1-1.py
@@ -1,5 +1,19 @@
 import networkx as nx
 
+'''
+BASICS PER A NTWX
+
+graf no dirigit -> g=nx.Graph()
+g dirigit -> g=nx.DiGraph()
+g en pesos -> G.add_edge(1,2,wieght=x)
+
+g.add_node() // g.add_nodes_from((llista))
+g.add_edge() // g.add_edges_from((llista))
+
+
+'''
+
+
 #primer funcio bulid graf del arxiu .csv
 
 def build_graph(nomarxiu):
@@ -15,10 +29,70 @@ def build_graph(nomarxiu):
 
 #programar funcio bfs
 def components_BFS(graf):
-    
-    llista_veins=[]
+    llista_recorreguda=set()
+    llista_final=[]
 
-    llista_recorreguda=[]
+    nodes=list(graf.nodes())
+
+    for node in nodes:
+        if node not in llista_recorreguda:
+            
+            pendents=[]
+            pendents.append(node)
+            llista_recorreguda.add(node)
+
+            graf2=[]
+
+            while pendents:
+                actual=pendents[0]
+                graf2.append(actual)
+                pendents.pop(0)
+
+                for vei in graf.neighbors(actual):
+                    if vei not in llista_recorreguda:
+                        llista_recorreguda.add(vei)
+                        pendents.append(vei)
+            
+            llista_final.append(graf2)
+    
+    return llista_final
+
+
+def components_DFS(graf):
+    llista_recorreguda=set()
+    llista_final=[]
+
+    nodes=list(graf.nodes())
+
+    for node in nodes:
+        if node not in llista_recorreguda:
+            
+            pendents=[]
+            pendents.append(node)
+            llista_recorreguda.add(node)
+
+            graf2=[]
+
+            while pendents:
+                actual=pendents[0]
+                graf2.append(actual)
+                pendents.pop()
+
+                for vei in graf.neighbors(actual):
+                    if vei not in llista_recorreguda:
+                        llista_recorreguda.add(vei)
+                        pendents.append(vei)
+            
+            llista_final.append(graf2)
+    
+    return llista_final
+
+
+print(build_graph('lastfm_asia_edges.csv'))
+
+
+
+
 
     
 
