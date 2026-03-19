4ddd696bde8e3784f2aecedfb8302091ed70d030
lorenaobiol <lorenacurto7@gmail.com>
2026-03-19 12:20:35 +0100

    Create practica2.py

---
+++ Practiques/P2/practica2.py
@@ -0,0 +1,25 @@
+import networkx as nx
+
+'''
+BASICS PER A NTWX
+
+graf no dirigit -> g=nx.Graph()
+g dirigit -> g=nx.DiGraph()
+g en pesos -> G.add_edge(1,2,wieght=x)
+
+g.add_node() // g.add_nodes_from((llista))
+g.add_edge() // g.add_edges_from((llista))
+
+
+'''
+ 
+def cliques(graf):
+
+    
+
+
+
+
+
+
+#ret subgrafs
